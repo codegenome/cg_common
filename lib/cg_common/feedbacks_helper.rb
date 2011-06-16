fd6e6f43e3f75734b478f764f4c7e53283fe32bc
Nicolas Buduroi <nbuduroi@gmail.com>
2011-06-15 20:12:05 -0400

    Fixed some issues.

--- lib/cg_common/feedbacks_helper.rb
+++ lib/cg_common/feedbacks_helper.rb
@@ -12,18 +12,18 @@ module CgCommon
         :reported_from  => from
       }
 
-      link_to "feedback", feedback_url(feedback_params), :popup => "400x300"
+      link_to "feedback", _cg_feedback_url(feedback_params), :popup => "400x300"
     end
 
     private
 
-    def feedback_url(feedback_params)
+    def _cg_feedback_url(feedback_params)
       config = CgCommon::CgProjectConfig.new(Rails.root)
       feedback_params.merge! :project_token => config.project_token
 
       config.site_url +
         '/extranet/projects/' +
-        config.project_id +
+        config.project_id.to_s +
         '/stories?' +
         feedback_params.to_param
     end
