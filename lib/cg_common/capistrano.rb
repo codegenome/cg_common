450525b698161dd77039e06b868b299b1861ba7b
Nicolas Buduroi <nbuduroi@gmail.com>
2011-06-28 19:52:10 -0400

    Renamed `recipes` to `capistrano` and added the `github_footer` task, placed it into the new `cg` namespace and renamed it `version_footer`.

--- lib/cg_common/capistrano.rb
+++ lib/cg_common/capistrano.rb
@@ -2,6 +2,22 @@
 
 Capistrano::Configuration.instance.load do
 
+  before "deploy:symlink", "cg:version_footer"
+
+  namespace :cg do
+
+    desc "Creates layouts/footer/_version partial view."
+    task :version_footer do
+      revision_url = "https://github.com/codegenome/#{application}/commit/#{real_revision}"
+      html = address +
+        " - version #{version}.#{stage}" +
+        " - #{Time.now.strftime("%d/%m/%y %I:%M%p %Z")}" +
+        " - rev. <a href='#{revision_url}' target='_blank'>#{real_revision[0..6] + "..."}</a>"
+      run "echo \"#{html}\"  > #{release_path}/app/views/layouts/footer/_version.html.erb"
+    end
+
+  end
+
   namespace :db do
 
     desc "Load production data into development database"
