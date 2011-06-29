2e9c45c507b72051763a23f5e696d55d964aa0b6
Nicolas Buduroi <nbuduroi@gmail.com>
2011-06-28 20:12:52 -0400

    Added the custom web:disable task from cg-project.

--- lib/cg_common/capistrano.rb
+++ lib/cg_common/capistrano.rb
@@ -65,4 +65,23 @@ Capistrano::Configuration.instance.load do
 
   end
 
+  namespace :deploy do
+
+    namespace :web do
+
+      desc "Present a maintenance page to visitors."
+      task :disable, :roles => :web do
+        on_rollback { rm "#{shared_path}/system/maintenance.html" }
+
+        require 'erb'
+        deadline, reason = ENV['UNTIL'], ENV['REASON']
+        maintenance = ERB.new(File.read("./app/views/layouts/maintenance.erb")).result(binding)
+
+        put maintenance, "#{shared_path}/system/maintenance.html", :mode => 0644
+      end
+
+    end
+
+  end
+
 end
