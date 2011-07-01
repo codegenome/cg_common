82787fe62b0caf00346ebc661721f1a780d068c0
Nicolas Buduroi <nbuduroi@gmail.com>
2011-07-01 12:35:21 -0400

    Made the disable recipe use erubis instead of erb.

--- lib/cg_common/recipes/deploy.rb
+++ lib/cg_common/recipes/deploy.rb
@@ -4,11 +4,12 @@ Capistrano::Configuration.instance.load do
 
       desc "Present a maintenance page to visitors."
       task :disable, :roles => :web do
+        require 'erubis'
+
         on_rollback { rm "#{shared_path}/system/maintenance.html" }
 
-        require 'erb'
         deadline, reason = ENV['UNTIL'], ENV['REASON']
-        maintenance = ERB.new(File.read("./app/views/layouts/maintenance.erb")).result(binding)
+        maintenance = Erubis::Eruby.new("./app/views/layouts/maintenance.erb").result(binding)
 
         put maintenance, "#{shared_path}/system/maintenance.html", :mode => 0644
       end
