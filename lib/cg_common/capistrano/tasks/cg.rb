8e700dffb5baa31bc543170294aa7cd8d88b527b
Nicolas Buduroi <nbuduroi@gmail.com>
2011-07-07 13:11:34 -0400

    Fixed bug when the path for the version_footer task is inexistent.

--- lib/cg_common/capistrano/tasks/cg.rb
+++ lib/cg_common/capistrano/tasks/cg.rb
@@ -6,11 +6,15 @@ Capistrano::Configuration.instance.load do
     desc "Creates layouts/footer/_version partial view."
     task :version_footer do
       revision_url = "https://github.com/codegenome/#{application}/commit/#{real_revision}"
+
       html = address +
         " - version #{version}.#{stage}" +
         " - #{Time.now.strftime("%d/%m/%y %I:%M%p %Z")}" +
         " - rev. <a href='#{revision_url}' target='_blank'>#{real_revision[0..6] + "..."}</a>"
-      run "echo \"#{html}\"  > #{release_path}/app/views/layouts/footer/_version.html.erb"
+
+      path = "app/views/layouts/footer"
+      run "mkdir -p #{release_path}/#{path}"
+      run "echo \"#{html}\"  > #{release_path}/#{path}/_version.html.erb"
     end
 
   end
