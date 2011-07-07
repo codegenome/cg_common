5d9dbbd52368f4f2dcb0f583060a951031129c19
Nicolas Buduroi <nbuduroi@gmail.com>
2011-07-07 12:53:10 -0400

    Fixed Ticket #05308 version_footer - y a t-il moyen que si je met ce partial dans le layout de mon app: render :partial => "footer/version" la gem "detecte" que le fichier n'existe pas et affiche un partial par defaut (surtout pour le DEV)

--- lib/cg_common.rb
+++ lib/cg_common.rb
@@ -1,5 +1,6 @@
 
 module CgCommon
+  GEM_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 end
 
 if defined?(::Rails::Railtie)
--- lib/cg_common/railtie.rb
+++ lib/cg_common/railtie.rb
@@ -7,6 +7,12 @@ module CgCommon
       ActionView::Base.send :include, CgCommon::Helpers
     end
 
+    initializer "cg_common.action_controller" do |app|
+      gem_view_path =
+        File.expand_path(File.join(CgCommon::GEM_ROOT, 'app', 'views'))
+      ActionController::Base.append_view_path gem_view_path
+    end
+
     initializer "cg_common.action_mailer" do |app|
       require "cg_common/development_mail_interceptor"
       ActionMailer::Base.register_interceptor DevelopmentMailInterceptor if Rails.env.development?
